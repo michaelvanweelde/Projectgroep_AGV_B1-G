14e54f7436adb37b478ad1b74dfcfa027e31a9ce
MichaelvanWeelde <Michaelvanweelde@gmail.com>
2021-05-11 15:40:08 +0200

    Added additional comments and further setup

--- AGV_code/main.c
+++ AGV_code/main.c
@@ -12,62 +12,123 @@
 #define motorX_BRK PORTA &= ~(1<<PA0 | 1<<PA1)              // Stop the X dir motor
 ----------------------------------*/
 
+int north_angle = 0;
+int UltrasoonRead(int sensor);
+int MagnometerRead();
+
 int main(void)
 {
 
 //-Initialise Sensors------
-/*
-Cny70's
-setup pins
+    /*
+    Cny70's
+    setup pins
 
-ultrasoon
-setup pins
+    ultrasoon
+    setup pins
 
-TCS32000's
-setup pins
-set prescaler
-*/
+    TCS32000's
+    setup pins
+    set prescaler
+    */
 //-------------------------
 
 
 //-Initialise motors-------
-/*
-setup pins
-setup PWM signals
-*/
+    /*
+    setup pins
+    setup PWM signals
+    */
 //-------------------------
 
 
 //-Initialise I2C interface
-/*
-Setup I2C interface with TWI builtin
-*/
+    /*
+    Setup I2C interface with TWI builtin
+    */
 //-------------------------
 
 
 //-Initialise Serial-------
-/*
-setup serial data line
-send serial RDY over line
-*/
+    /*
+    setup serial data line
+    send serial RDY over line
+    */
 //-------------------------
 
 
 //-Setup timer for delays--
-/*
-Setup timer with a +- 1ms overflow time
-*/
+    /*
+    Setup timer with a +- 1ms overflow time
+    */
 //-------------------------
 
-sei();
+
+//-zero magnometer---------
+north_angle = MagnometerRead();
+//-------------------------
 
 
+    sei();
 
-    while(1){
+//-End of setup-----------------------------------------]
+    while(1)
+    {
 
 
 
 
     }
     return 0;
+}
+
+int UltrasoonRead(int sensor)
+{
+
+    //-initialize variables-------
+    volatile float echo=0.0;
+    int timeout=0;
+    //----------------------------
+
+
+    //-Pulse the Trig pin---------
+    //----------------------------
+
+
+    //-Wait until echo turns high-
+    //----------------------------
+
+
+    //-then measure pulse width---
+    //----------------------------
+
+
+    return echo;
+}
+
+
+int MagnometerRead(){
+int Xvalue;
+int Yvalue;
+int Zvalue;
+
+    //-Read X value and write to mem
+    //----------------------------
+
+    //-Read Y value and write to mem
+    //----------------------------
+
+    //-Read Z value and write to mem
+    //----------------------------
+
+    //-compare the data-----------
+    /*
+    with trigonometry make sure that
+    the center point of all 3 sensors
+    is not close to the vehicle
+    this way magnets are ignored
+    */
+    //----------------------------
+
+return Xvalue;
 }
